f7bbdf78de5f45b2b2a4313a9255bee181309fb2
mamingyang <1624133181@qq.com>
2026-02-08 21:34:59 +0800

    fix

--- wiib-web/src/pages/Login.tsx
+++ wiib-web/src/pages/Login.tsx
@@ -7,16 +7,16 @@ import { Card, CardContent } from '../components/ui/card';
 import { Typewriter } from '../components/ui/typewriter';
 import { TrendingUp, Loader2, Globe, BarChart3, Wallet, LineChart } from 'lucide-react';
 
-// const LINUXDO_CONFIG = {
-//   clientId: 'toCFytIO9bCHpbUbFKM1mTgvy1ax8tG2',
-//   authorizeUrl: 'https://connect.linux.do/oauth2/authorize',
-//   redirectUri: 'https://linuxdo.stockgame.icu/login',
-// };
 const LINUXDO_CONFIG = {
-  clientId: 'NIrMpQ09Jgzjb7r1ZgU3QYnuejk8Z3qS',
+  clientId: 'toCFytIO9bCHpbUbFKM1mTgvy1ax8tG2',
   authorizeUrl: 'https://connect.linux.do/oauth2/authorize',
-  redirectUri: 'http://localhost:3000/login',
+  redirectUri: 'https://linuxdo.stockgame.icu/login',
 };
+// const LINUXDO_CONFIG = {
+//   clientId: 'NIrMpQ09Jgzjb7r1ZgU3QYnuejk8Z3qS',
+//   authorizeUrl: 'https://connect.linux.do/oauth2/authorize',
+//   redirectUri: 'http://localhost:3000/login',
+// };
 
 export function Login() {
   const navigate = useNavigate();
